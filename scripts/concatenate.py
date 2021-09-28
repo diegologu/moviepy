716ce13691c8413ab3eff9419aebb61d0f55b762
diegolop08 <diegolopezg01@hotmail.com>
2021-09-27 21:10:58 -0500

    Final Commit

--- scripts/concatenate.py
+++ scripts/concatenate.py
@@ -2,6 +2,8 @@ import sys
 import moviepy.editor as mpy
 
 main = mpy.VideoFileClip("output/video_array.mp4")
+main = mpy.VideoFileClip("output/message.mp4")
+main = main.resize((1920,1080))
 credits = mpy.VideoFileClip("input/credits.mp4")
 final = mpy.concatenate_videoclips([main,credits])
 final.write_videofile(sys.argv[2])
